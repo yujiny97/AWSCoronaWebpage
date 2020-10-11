038cd90e3b045762b8153984c77fbf9b45e4d6bf
yujin <i5533m@naver.com>
2020-10-11 20:45:21 +0900

    commit

---
+++ AWSCoronaWebpage/src/main/java/com/awsteam/app/AwsCoronaWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.awsteam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCoronaWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCoronaWebpageApplication.class, args);
+	}
+
+}
---
+++ AWSCoronaWebpage/src/main/webapp/WEB-INF/include/header.jsp
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="/aws/bootstrap/css/bootstrap.css" >
+<link rel="stylesheet" href="/aws/bootstrap/css/bootstrap-theme.css" >
+<script src="/aws/bootstrap/js/bootstrap.js"></script>
+zzzzzzz
